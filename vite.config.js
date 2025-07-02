7b8995b4e949da453cdf64c9a86920193aff32b8
Your Name <your@email.com>
2025-07-02 01:06:09 -0700

    rec commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Do not add 'base' here unless deploying under a subdirectory
 })
